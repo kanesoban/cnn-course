276fa6703b7829bbe30d11004e5a77302699a0c4
kanesoban <kanesoban@gmail.com>
2018-06-24 11:36:55 +0200

    Tensorflow CNN implementation

---
+++ cnn_tf.py
@@ -0,0 +1,81 @@
+import numpy
+import tensorflow as tf
+
+
+class DenseLayer:
+    def __init__(self, input_var, output_size, nonlinearity=None):
+        self.input_var = input_var
+        self.weights = tf.Variable(numpy.zeros((input_var.shape[1], output_size), dtype=numpy.float32))
+        self.bias = tf.Variable(numpy.zeros((1,), dtype=numpy.float32))
+        self.nonlinearity = nonlinearity
+        self.op = self.nonlinearity(tf.matmul(self.input_var, self.weights) + self.bias)
+        if self.nonlinearity:
+            self.op = self.nonlinearity(self.op)
+        self.shape = self.op.shape
+
+
+class ConvPoolLayer:
+    def __init__(self, input_var, conv_size, filters_out, pool_size, nonlinearity=None):
+        self.width = input_var.shape[1]
+        self.heigth = input_var.shape[2]
+        self.filters_in = input_var.shape[3]
+        self.filters_out = filters_out
+        self.input_var = input_var
+        self.pool_size = pool_size
+        self.weights = tf.Variable(numpy.zeros((conv_size, conv_size, self.filters_in, self.filters_out),
+                                               dtype=numpy.float32))
+        self.bias = tf.Variable(numpy.zeros((1,), dtype=numpy.float32))
+        self.nonlinearity = nonlinearity
+
+        self.op = tf.nn.conv2d(self.input_var, self.weights, strides=[1, 1, 1, 1], padding='VALID')
+        self.op = self.op + self.bias
+        self.op = tf.nn.max_pool(
+            self.op,
+            ksize=[1, self.pool_size, self.pool_size, 1],
+            strides=[1, self.pool_size, self.pool_size, 1],
+            padding='VALID'
+        )
+        if self.nonlinearity:
+            self.op = self.nonlinearity(self.op)
+        self.shape = self.op.shape
+
+
+class LeNet5:
+    def __init__(self, input_size=(32, 32, 3), classes=2, learning_rate=1e-3):
+        self.classes = classes
+        self.learning_rate = learning_rate
+        self.session = tf.Session()
+        width, height, color_channels = input_size
+        with tf.variable_scope('LeNet5', reuse=tf.AUTO_REUSE):
+            self.layers = []
+            self.input_var = tf.placeholder(dtype=tf.float32, shape=(None, width, height, color_channels), name='input_var')
+            self.target_var = tf.placeholder(dtype=tf.float32, shape=(None, self.classes), name='labels_var')
+            self.layers.append(ConvPoolLayer(self.input_var, conv_size=5, filters_out=6, pool_size=2,
+                                             nonlinearity=tf.nn.tanh))
+            self.layers.append(ConvPoolLayer(self.layers[-1].op, conv_size=5, filters_out=16, pool_size=2,
+                                             nonlinearity=tf.nn.tanh))
+            flattened_input = tf.layers.flatten(self.layers[-1].op)
+            #flattened_input = tf.layers.flatten(self.input_var)
+            self.layers.append(DenseLayer(flattened_input, 120, nonlinearity=tf.nn.relu).op)
+            self.layers.append(DenseLayer(self.layers[-1], 84, nonlinearity=tf.nn.relu).op)
+            self.layers.append(DenseLayer(self.layers[-1], 2, nonlinearity=tf.nn.relu).op)
+
+        self.forward_op = self.layers[-1]
+
+        self.cost = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits_v2(
+                logits=self.forward_op,
+                labels=self.target_var
+            )
+        )
+        self.train_op = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.cost)
+        self.session.run(tf.global_variables_initializer())
+
+    def fit(self, data, targets):
+        return self.session.run(self.train_op, feed_dict={self.input_var: data, self.target_var: targets})
+
+    def predict(self, data):
+        return numpy.argmax(self.session.run(self.layers[-1], feed_dict={self.input_var: data}))
+
+    def loss(self, data, targets):
+        return self.session.run(self.cost, feed_dict={self.input_var: data, self.target_var: targets})
--- facial_recognizer.py
+++ facial_recognizer.py
@@ -1,38 +1,38 @@
 import h5py
 import sys
+import numpy
 from tqdm import tqdm
 import matplotlib.pyplot as plot
 
-from cnn import DenseLayer
-from cnn import ConvPoolLayer
 from cnn import LeNet5
 
-
-epochs = 100
+epochs = 10
 t = 0.8
 
-with h5py.File(sys.argv[1]) as f:
-	data = f['data'][()]
-	targets = f['targets'][()]
+with h5py.File(sys.argv[1], 'r') as f:
+    data = f['data'][()]
+    targets = f['targets'][()]
 
-train_data = data[:int(len(data.shape[0]) * t)]
-train_targets = targets[:int(len(data.shape[0]) * t)]
-test_data = data[int(len(data.shape[0]) * t):]
-test_targets = data[int(len(data.shape[0]) * t):]
+data = data / 255.0
 
-model = LeNet5(2, 1, 0.001)
+train_data = data[:int(data.shape[0] * t)]
+train_targets = targets[:int(data.shape[0] * t)]
+test_data = data[int(data.shape[0] * t):]
+test_targets = targets[int(data.shape[0] * t):]
+
+model = LeNet5(input_size=(48, 48, 1), classes=2)
 
 losses = []
 prev_loss = numpy.inf
 prev_model = model
 for epoch in tqdm(range(epochs), desc='Epoch'):
-	model.fit(train_data, train_targets)
-	if epoch % 5 == 0:
-		loss = model.loss(test_data, test_targets)
-		losses.append(loss)
-		if loss > prev_loss:
-			break
-		prev_loss = loss
+    model.fit(train_data, train_targets)
+    if epoch % 5 == 0:
+        loss = model.loss(test_data, test_targets)
+        losses.append(loss)
+        if loss > prev_loss:
+            break
+        prev_loss = loss
 
-plot.plot(loss)
+plot.plot(losses)
 plot.savefig('losses.png')
